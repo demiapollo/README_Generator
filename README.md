740985c4505c351a7c07bae3d3cf6b19345258f0
demiapollo <apollodemirel67@gmail.com>
2023-04-10 10:40:05 -0700

    errors fixed.

---
+++ README.md
@@ -0,0 +1,23 @@
+## Table of Contents
+[Description](#description:)
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributing](#contributing)
+[Tests](#tests)
+[Questions](#questions)
+
+## Description: 
+q,
+## Installation: 
+r,
+## Usage: 
+t,
+## License: 
+MIT,
+## Contributing: 
+h,
+## Tests: 
+n,
+## Questions: 
+b,
--- index.js
+++ index.js
@@ -1,83 +1,112 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // TODO: Create an array of questions for user input
-const questions = [];
-inquirer.prompt ([
+// const questions = [];
+
+function generateREADME({
+  description,
+  installation,
+  usage,
+  license,
+  contributing,
+  tests,
+  questions,
+}) {
+  return `## Table of Contents
+[Description](#description:)
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributing](#contributing)
+[Tests](#tests)
+[Questions](#questions)
+
+## Description: 
+${description},
+## Installation: 
+${installation},
+## Usage: 
+${usage},
+## License: 
+${license},
+## Contributing: 
+${contributing},
+## Tests: 
+${tests},
+## Questions: 
+${questions},
+`;
+}
+
+inquirer
+  .prompt([
     {
-    title: 'title',
-    message: 'What is the title of your project?',
-    type: 'input'
+      name: "title",
+      message: "What is the title of your project?",
+      type: "input",
     },
 
     {
-    title: 'description',
-    message: 'What is the description of your project?',
-    type: 'input'
+      name: "description",
+      message: "What is the description of your project?",
+      type: "input",
     },
 
     {
-    title: 'table of contents',
-    message: 'What is included in the table of contents of your project?',
-    type: 'list',
-    choices: ['Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions'],
+      name: "installation",
+      message: "How to install of your project?",
+      type: "input",
     },
 
     {
-    title: 'installation',
-    message: 'How to install of your project?',
-    //type: 'input' ? just a message
+      name: "usage",
+      message: "How can we use your project?",
+      type: "input",
     },
 
     {
-    title: 'usage',
-    message: 'How can we use your project?',
-    type: 'input'
+      name: "license",
+      message: "What license is used for your project?",
+      type: "list",
+      choices: ["MIT", "Apache", "GPL", "BSD", "None"],
     },
 
     {
-    title: 'license',
-    message: 'What license is used for your project?',
-    type: 'list',
-    choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+      name: "contributing",
+      message: "Who has contributed to your project?",
+      type: "input",
     },
 
     {
-    title: 'contributing',
-    message: 'Who has contributed to your project?',
-    type: 'input'
+      name: "tests",
+      message: "How to test your project?",
+      type: "input",
     },
 
     {
-    title: 'tests',
-    message: 'How to test your project?',
-    type: 'input'
+      name: "questions",
+      message: "What is your GitHub username?",
+      type: "input",
     },
-
-    {
-    title: 'questions',
-    message: 'What is your GitHub username?',
-    type: 'input'
-    },
-
-]).then((answers => {
+  ])
+  .then((answers) => {
     console.log(answers);
 
-    fs.watchFile('README.md', answers, (err) => {
-        if (err) throw err;
-        console.log('success');
-    })
-}));
+    const generateREADMEContent = generateREADME(answers);
 
+    fs.writeFile("README.md", generateREADMEContent, (err) => {
+      if (err) throw err;
+      console.log("success");
+    });
+  });
 
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
+// // TODO: Create a function to initialize app
+// function init() {}
 
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
